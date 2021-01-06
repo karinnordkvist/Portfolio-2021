0fdc477d2d6be841b04bb9d1df9d48ba673b44c1
Karin Nordkvist <kontakt@karinnordkvist.se>
2021-01-06 13:37:57 +0100

    Changed OG-address

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,18 +1,9 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
-export const Contact = ({ offset }) => {
-  const wrapperRef = useRef();
-  const [currentOffset, setCurrentOffset] = useState();
-  const [currentHeight, setCurrentHeight] = useState();
-
-  useEffect(() => {
-    setCurrentOffset(wrapperRef.current.offsetTop);
-    setCurrentHeight(wrapperRef.current.scrollHeight);
-  }, []);
-
+export const Social = () => {
   return (
-    <ContactWrapper
+    <SocialWrapper
       offset={offset}
       ref={wrapperRef}
       currentOffset={currentOffset}
@@ -23,21 +14,21 @@ export const Contact = ({ offset }) => {
         Send me an email <a href="mailto:kontakt@karinnordkvist.se">here</a>!
       </ContactText>
       <ContactText>+46 721 77 27 77</ContactText>
-    </ContactWrapper>
+    </SocialWrapper>
   );
 };
 
-const ContactWrapper = styled.div`
+const SocialWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   flex-direction: column;
   background: ${(props) =>
-    props.offset < props.currentOffset - props.currentHeight
+    props.offset < props.currentOffset - props.currentHeight * 2
       ? '#fff'
       : '#f4f0ec'};
   opacity: ${(props) =>
-    props.offset < props.currentOffset - props.currentHeight ? '0' : '1'};
+    props.offset < props.currentOffset - props.currentHeight * 2 ? '0' : '1'};
   padding: 100px;
   transition: all 1s ease-in-out;
 `;
